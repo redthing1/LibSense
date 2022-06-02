9a668c8c5a399ccd997aa30326602e64271c6230
redthing1 <redthing1@alt.icu>
2022-06-02 12:02:22 -0700

    add summarizer route

--- src/doccrunch/doccrunch/server.py
+++ src/doccrunch/doccrunch/server.py
@@ -147,6 +147,80 @@ def gen_bart_classifier_route(ext):
         abort(400, f"generation fembed_ailed")
 
 
+@route("/gen_bart_summarizer.<ext>", method=["GET", "POST"])
+def gen_bart_summarizer_route(ext):
+    req_json = req_as_dict(request)
+    try:
+        _ = req_json["text"]
+    except KeyError as ke:
+        abort(400, f"missing field {ke}")
+
+    # mode params
+    # option params
+    opt_text: str = get_req_opt(req_json, "text", "")
+    opt_max_length: int = get_req_opt(req_json, "max_length", 256)
+    opt_min_length: int = get_req_opt(req_json, "min_length", 0)
+    opt_typical_p: float = get_req_opt(req_json, "typical_p", 0.9)
+    opt_repetition_penalty: float = get_req_opt(req_json, "repetition_penalty", 1.0)
+    opt_length_penalty: float = get_req_opt(req_json, "length_penalty", 1.0)
+    opt_max_time: float = get_req_opt(req_json, "opt_max_time", None)
+    opt_no_repeat_ngram_size: int = get_req_opt(req_json, "no_repeat_ngram_size", 3)
+
+    logger.debug(f"requesting generation for text: {opt_text}")
+
+    # generate
+    try:
+        start = time.time()
+
+        global AI_SUMMARIZER_INSTANCE, SUMMARY_GENERATOR
+
+        # standard generate
+        output = SUMMARY_GENERATOR.generate(
+            article=opt_text,
+            max_length=opt_max_length,
+            min_length=opt_min_length,
+            typical_p=opt_typical_p,
+            num_beams=1,  # disable beam search
+            do_sample=False,
+            repetition_penalty=opt_repetition_penalty,
+            length_penalty=opt_length_penalty,
+            max_time=opt_max_time,
+            no_repeat_ngram_size=opt_no_repeat_ngram_size,
+        )
+
+        gen_txt = AI_SUMMARIZER_INSTANCE.filter_text(output.text)
+        gen_txt_size = len(gen_txt)
+        prompt_token_count = len(output.prompt_ids)
+        logger.debug(f"model output: {gen_txt}")
+        generation_time = time.time() - start
+        total_gen_num = len(output.tokens)
+        gen_tps = output.num_new / generation_time
+        logger.info(
+            f"generated [{prompt_token_count}->{output.num_new}] ({generation_time:.2f}s/{(gen_tps):.2f}tps)"
+        )
+
+        # done generating, now return the results over http
+
+        # create base response bundle
+        resp_bundle = {
+            "text": gen_txt,
+            "text_length": gen_txt_size,
+            "prompt_token_count": prompt_token_count,
+            "tokens": output.tokens,
+            "token_count": total_gen_num,
+            "num_new": output.num_new,
+            "num_total": total_gen_num,
+            "gen_time": generation_time,
+            "gen_tps": gen_tps,
+            "model": AI_SUMMARIZER_INSTANCE.model_name,
+        }
+
+        return pack_bundle(resp_bundle, ext)
+    except Exception as ex:
+        logger.error(f"error generating: {traceback.format_exc()}")
+        abort(400, f"generation failed")
+
+
 def run_server(embed_ai, summarizer_ai, host: str, port: int, debug: bool):
     global AI_EMBED_INSTANCE, EMBED_GENERATOR, AI_SUMMARIZER_INSTANCE, SUMMARY_GENERATOR
 

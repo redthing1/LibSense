ae570b4f82b48f3825b8791ecd130f857f5e4f98
redthing1 <redthing1@alt.icu>
2022-06-03 01:34:51 -0700

    stub searcher

--- src/sensetool/source/searching.d
+++ src/sensetool/source/searching.d
@@ -1,5 +1,19 @@
 module searching;
 
+import global;
+import config;
+
+struct SearchResult {
+}
+
 class LibrarySearcher {
-    
+    LibSenseConfig config;
+
+    this(LibSenseConfig config) {
+        this.config = config;
+    }
+
+    public SearchResult[] search() {
+        return [SearchResult()];
+    }
 }

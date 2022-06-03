8f4134e86b10874891352dbaba7f19f980fa3e51
redthing1 <redthing1@alt.icu>
2022-06-02 22:31:23 -0700

    create embeddings of documents

--- src/sensetool/source/docprocess.d
+++ src/sensetool/source/docprocess.d
@@ -98,7 +98,46 @@ class DocumentProcessor {
             summaries ~= summary_data.text;
         }
 
-        auto processed_doc = ProcessedDocument(input_doc.key, document_sents, summaries);
+        // create embeddings for every sentence and every summary
+        struct EmbedReq {
+            string[] texts;
+        }
+
+        struct EmbedResp {
+            TEmbedding[] embeds;
+        }
+
+        enum EMBED_BATCH_SIZE = 64;
+        auto sent_embed_chunks = document_sents.chunk!string(x => x.length >= EMBED_BATCH_SIZE);
+        auto summary_embed_chunks = summaries.chunk!string(x => x.length >= EMBED_BATCH_SIZE);
+
+        bool accumulate_embed_chunks(ref Appender!(TEmbedding[]) embeds, ref string[][] chunks) {
+            foreach (i, chunk; chunks) {
+                auto embed_req = EmbedReq(chunk);
+                auto embed_resp = client.post!(EmbedReq, EmbedResp)(
+                    backend_url ~ "/gen_sentence_embed.json", embed_req);
+                if (embed_resp == none) {
+                    log.err(format("failed to embed document: %s", input_doc.key));
+                    return false;
+                }
+                auto embed_data = embed_resp.front;
+                log.trace(format("embedded %s chunk #%d",
+                        input_doc.key, i));
+                embeds ~= embed_data.embeds;
+            }
+            return true;
+        }
+
+        auto sent_embeddings = appender!(TEmbedding[]);
+        auto summ_embeddings = appender!(TEmbedding[]);
+
+        if (!accumulate_embed_chunks(sent_embeddings, sent_embed_chunks)
+            || !accumulate_embed_chunks(summ_embeddings, summary_embed_chunks)) {
+            return no!ProcessedDocument;
+        }
+
+        auto processed_doc = ProcessedDocument(input_doc.key, document_sents,
+            summaries, sent_embeddings.data, summ_embeddings.data);
         return some(processed_doc);
     }
 }
--- src/sensetool/source/models.d
+++ src/sensetool/source/models.d
@@ -6,9 +6,14 @@ struct FoundDocument {
     string raw_contents;
 }
 
+alias TEmbedding = float[];
+
 struct ProcessedDocument {
     string key;
-    
+
     string[] sentences;
     string[] summaries;
+
+    TEmbedding[] sentence_embeddings;
+    TEmbedding[] summary_embeddings;
 }

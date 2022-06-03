2b6be831dc9fedc7df731f69cf559614369f09c2
redthing1 <redthing1@alt.icu>
2022-06-03 01:26:00 -0700

    add tostring for founddocument

--- src/sensetool/source/models.d
+++ src/sensetool/source/models.d
@@ -4,6 +4,12 @@ struct FoundDocument {
     string key;
     string file_name;
     string raw_contents;
+
+    string toString() {
+        import std.format;
+        
+        return format("FoundDocument(key: %s, file_name: %s)", key, file_name);
+    }
 }
 
 alias TEmbedding = float[];

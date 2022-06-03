8197b3b5586afced6c0a4f79d10f25a4ef0fcf56
redthing1 <redthing1@alt.icu>
2022-06-02 17:07:24 -0700

    deserialize cleaned document data

--- src/sensetool/source/docprocess.d
+++ src/sensetool/source/docprocess.d
@@ -1,9 +1,15 @@
 module docprocess;
 
+import std.stdio;
 import std.format;
+import std.datetime;
+
 import requests;
 import mir.ser.json : serializeJson;
+import mir.deser.json : deserializeJson;
+import optional;
 
+import global;
 import models;
 
 class DocumentProcessor {
@@ -12,9 +18,10 @@ class DocumentProcessor {
         this.backend_url = backend_url;
     }
 
-    ProcessedDocument process_document(FoundDocument input_doc) {
+    Optional!ProcessedDocument process_document(FoundDocument input_doc) {
         // clean the document
         auto clean_req = Request();
+        clean_req.timeout = 600.seconds;
         struct CleanReq {
             string contents;
             int max_sentence_length;
@@ -23,16 +30,24 @@ class DocumentProcessor {
 
         auto clean_req_json = CleanReq(input_doc.raw_contents, 2000, true).serializeJson();
 
+        log.info(format("cleaning document: %s", input_doc.key));
         auto clean_resp = clean_req.post(backend_url ~ "/clean_document.json",
             clean_req_json,
             "application/json"
         );
+        if (clean_resp.code != 200) {
+            return no!ProcessedDocument;
+        }
+        struct CleanResp {
+            string[] sents;
+            int num_sents;
+            int num_initial_sents;
+        }
+        auto clean_resp_data = clean_resp.responseBody.data!string.deserializeJson!CleanResp();
+        writefln("clean_resp_data: %s", clean_resp_data);
+        writefln("num sents: %s", clean_resp_data.num_sents);
 
-        import std.stdio;
-
-        writefln("clean_resp: %s", clean_resp);
-
-        auto processed_doc = ProcessedDocument(input_doc.key, []);
-        return processed_doc;
+        auto processed_doc = ProcessedDocument(input_doc.key, clean_resp_data.sents);
+        return some(processed_doc);
     }
 }
